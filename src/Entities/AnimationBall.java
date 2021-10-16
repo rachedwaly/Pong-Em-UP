09700d9dff975d184b10fefca8915eba0e04d636
Rached Waly <rachedwaly1998@gmail.com>
2021-10-16 13:04:09 +0100

    animation on the main menu fix

--- src/Entities/AnimationBall.java
+++ src/Entities/AnimationBall.java
@@ -1,7 +1,7 @@
 package Entities;
 
 import Game.Model;
-
+// I created this class to ovverride the move method for animation purposes
 public class AnimationBall extends Ball{
     public AnimationBall(int x, int y, Model model) {
         super(x, y, model);
