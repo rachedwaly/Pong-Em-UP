d3a225029ce6bca50d3c746dd063d559da39896f
Rached Waly <rachedwaly1998@gmail.com>
2021-10-21 12:53:54 +0100

    marking the resources folder as resource root

--- src/AltLib/ImageLoader.java
+++ src/AltLib/ImageLoader.java
@@ -6,6 +6,7 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.Objects;
 
 /**
  * Loads images in a separate static class
@@ -29,35 +30,35 @@ public class ImageLoader {
 
     static {
         try {
-            healthImage = ImageIO.read(new File("Resources/health.png"));
+            healthImage = ImageIO.read(ImageLoader.class.getResource("/health.png"));
             bgImage = new BufferedImage[2];
-            bgImage[0] = ImageIO.read(new File("Resources/background lvl1.jpg"));
-            bgImage[1] = ImageIO.read(new File("Resources/background lvl2.jpg"));
-            cloudImage = ImageIO.read(new File("Resources/cloud.png"));
+            bgImage[0] = ImageIO.read((ImageLoader.class.getResource("/background lvl1.jpg")));
+            bgImage[1] = ImageIO.read(ImageLoader.class.getResource("/background lvl2.jpg"));
+            cloudImage = ImageIO.read(ImageLoader.class.getResource("/cloud.png"));
             explosionAnimation = new BufferedImage[9];
-            explosionAnimation[0] = ImageIO.read(new File("Resources/explosion_animation/1death.png"));
-            explosionAnimation[1] = ImageIO.read(new File("Resources/explosion_animation/2death.png"));
-            explosionAnimation[2] = ImageIO.read(new File("Resources/explosion_animation/3death.png"));
-            explosionAnimation[3] = ImageIO.read(new File("Resources/explosion_animation/4death.png"));
-            explosionAnimation[4] = ImageIO.read(new File("Resources/explosion_animation/5death.png"));
-            explosionAnimation[5] = ImageIO.read(new File("Resources/explosion_animation/6death.png"));
-            explosionAnimation[6] = ImageIO.read(new File("Resources/explosion_animation/7death.png"));
-            explosionAnimation[7] = ImageIO.read(new File("Resources/explosion_animation/8death.png"));
-            explosionAnimation[8] = ImageIO.read(new File("Resources/explosion_animation/9death.png"));
-            sentryImage = ImageIO.read(new File("Resources/sentry.png"));
-            sentryDamaged = ImageIO.read(new File("Resources/sentryDamaged.png"));
-            spinnerImage = ImageIO.read(new File("Resources/ufo.png"));
-            spinnerDamaged = ImageIO.read(new File("Resources/ufoDamaged.png"));
-            planeImage = ImageIO.read(new File("Resources/plane.png"));
-            gameoverImage = ImageIO.read(new File("Resources/gameover.png"));
-            shieldBonusImage = ImageIO.read(new File("Resources/shield.png"));
-            shieldStickImage = ImageIO.read(new File("Resources/shieldStick.png"));
-            muscleBonusImage = ImageIO.read(new File("Resources/muscle.png"));
+            explosionAnimation[0] = ImageIO.read(ImageLoader.class.getResource("/explosion_animation/1death.png"));
+            explosionAnimation[1] = ImageIO.read(ImageLoader.class.getResource("/explosion_animation/2death.png"));
+            explosionAnimation[2] = ImageIO.read(ImageLoader.class.getResource("/explosion_animation/3death.png"));
+            explosionAnimation[3] = ImageIO.read(ImageLoader.class.getResource("/explosion_animation/4death.png"));
+            explosionAnimation[4] = ImageIO.read(ImageLoader.class.getResource("/explosion_animation/5death.png"));
+            explosionAnimation[5] = ImageIO.read(ImageLoader.class.getResource("/explosion_animation/6death.png"));
+            explosionAnimation[6] = ImageIO.read(ImageLoader.class.getResource("/explosion_animation/7death.png"));
+            explosionAnimation[7] = ImageIO.read(ImageLoader.class.getResource("/explosion_animation/8death.png"));
+            explosionAnimation[8] = ImageIO.read(ImageLoader.class.getResource("/explosion_animation/9death.png"));
+            sentryImage = ImageIO.read(ImageLoader.class.getResource("/sentry.png"));
+            sentryDamaged = ImageIO.read(ImageLoader.class.getResource("/sentryDamaged.png"));
+            spinnerImage = ImageIO.read(ImageLoader.class.getResource("/ufo.png"));
+            spinnerDamaged = ImageIO.read(ImageLoader.class.getResource("/ufoDamaged.png"));
+            planeImage = ImageIO.read(ImageLoader.class.getResource("/plane.png"));
+            gameoverImage = ImageIO.read(ImageLoader.class.getResource("/gameover.png"));
+            shieldBonusImage = ImageIO.read(ImageLoader.class.getResource("/shield.png"));
+            shieldStickImage = ImageIO.read(ImageLoader.class.getResource("/shieldStick.png"));
+            muscleBonusImage = ImageIO.read(ImageLoader.class.getResource("/muscle.png"));
             stickImage=new BufferedImage[4];
-            stickImage[0]=ImageIO.read(new File("Resources/Stick/texture0.png"));
-            stickImage[1]=ImageIO.read(new File("Resources/Stick/texture1.png"));
-            stickImage[2]=ImageIO.read(new File("Resources/Stick/texture2.png"));
-            stickImage[3]=ImageIO.read(new File("Resources/Stick/texture3.png"));
+            stickImage[0]=ImageIO.read(ImageLoader.class.getResource("/Stick/texture0.png"));
+            stickImage[1]=ImageIO.read(ImageLoader.class.getResource("/Stick/texture1.png"));
+            stickImage[2]=ImageIO.read(ImageLoader.class.getResource("/Stick/texture2.png"));
+            stickImage[3]=ImageIO.read(ImageLoader.class.getResource("/Stick/texture3.png"));
         } catch (IOException e) {
             e.printStackTrace();
         }
